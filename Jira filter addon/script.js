ea069e1f875252c51b80d5e62b2f276b24c4583f
Jakub Szendzielorz <kubasz15@gmail.com>
2024-07-31 08:55:13 +0200

    refactoring and added filter by person

--- Jira filter addon/script.js
+++ Jira filter addon/script.js
@@ -1,89 +1,113 @@
 var jiraAddon = {
-  selectedFiltersOfType: [],
-  selectedFiltersOfStatus: [],
-  listOfElements: [],
-  arrayIssueStatus: [],
-  arrayIssueType: [],
-  listOfCheckboxesOfType: [],
-  listOfCheckboxesOfStatus: [],
-  init: function () {
-    jiraAddon.addingHtml();
-    jiraAddon.listOfElements = $($('.issuerow'));
-    jiraAddon.listOfElements.each(function (ind, el) {
+  selectedFilters: {
+    type: [],
+    status: [],
+    assignment: []
+  },
+  lists: {
+    elements: [],
+    issueStatus: [],
+    issueType: [],
+    issueAssignment: [],
+    checkboxesOfType: [],
+    checkboxesOfStatus: [],
+    checkboxesOfAssignment: []
+  },
+
+  init() {
+    this.addingHtml();
+    this.cacheDomElements();
+    this.populateIssueArrays();
+    this.attachFilterEventHandlers();
+  },
+
+  cacheDomElements() {
+    this.lists.elements = $('.issuerow');
+  },
+
+  populateIssueArrays() {
+    this.lists.elements.each((index, el) => {
       var issueStatus = $(el).find('.status span').text();
-      var isseuType = $(el).find('.issuetype a img').attr('alt');
-      jiraAddon.arrayIssueStatus[ind] = issueStatus;
-      jiraAddon.arrayIssueType[ind] = isseuType;
-      jiraAddon.addStatusHtml(issueStatus);
-      jiraAddon.addTypeHtml(isseuType);
-    });
-    var filterCheckboxesOfType = $('.checkboxesOfIssueType input[type="checkbox"]');
-    filterCheckboxesOfType.on('change', function (elem) {
-      jiraAddon.selectedFiltersOfType = [];
+      var issueType = $(el).find('.issuetype a img').attr('alt');
+      var issueAssignment = $(el).find('.assignee').text(); // Assuming the assignment is in a span within an element with class "assignee"
 
-      filterCheckboxesOfType.filter(':checked').each(function (j, el) {
-        jiraAddon.selectedFiltersOfType.push(el.value);
-      });
-      jiraAddon.filter();
-    });
-    var filterCheckboxesOfStatus = $('.checkboxesOfIssueStatus input[type="checkbox"]');
-    filterCheckboxesOfStatus.on('change', function (elem) {
-      jiraAddon.selectedFiltersOfStatus = [];
-      filterCheckboxesOfStatus.filter(':checked').each(function (j, el) {
-        jiraAddon.selectedFiltersOfStatus.push(el.value);
-      });
-      jiraAddon.filter();
+      this.lists.issueStatus[index] = issueStatus;
+      this.lists.issueType[index] = issueType;
+      this.lists.issueAssignment[index] = issueAssignment;
+
+      this.addFilterHtml('status', issueStatus);
+      this.addFilterHtml('type', issueType);
+      this.addFilterHtml('assignment', issueAssignment);
     });
   },
-  addingHtml: function () {
-    var jiraAddonContent =
-      '<div class="issues-wrap" style="padding-top: 16px;">' +
-      '<p style="font-size:16px;text-decoration: underline;"><strong>Issue Type:</strong></p>' +
-      '<form class="checkboxesOfIssueType">' +
-      '</form>' +
-      '<p style="font-size:16px;text-decoration: underline;"><strong>Issue Status:</strong></p>' +
-      '<form class="checkboxesOfIssueStatus">' +
-      '</form>' +
-      '</div>';
 
-    $("#greenhopper-agile-issue-web-panel_heading").append(jiraAddonContent);
-  },
-  addStatusHtml: function (issueStatus) {
-    if (jiraAddon.listOfCheckboxesOfStatus.indexOf(issueStatus) == -1) {
-      jiraAddon.listOfCheckboxesOfStatus.push(issueStatus);
-      $('.checkboxesOfIssueStatus').append(
-        '<label style="font-size:16px;">' +
-        '<input type="checkbox" name="issue-type" value="' + issueStatus + '"/> ' + issueStatus + '</label>' +
-        '<br>')
+  addFilterHtml(filterType, value) {
+    let checkboxesList;
+    let container;
+    
+    if (filterType === 'status') {
+      checkboxesList = this.lists.checkboxesOfStatus;
+      container = '.checkboxesOfIssueStatus';
+    } else if (filterType === 'type') {
+      checkboxesList = this.lists.checkboxesOfType;
+      container = '.checkboxesOfIssueType';
+    } else if (filterType === 'assignment') {
+      checkboxesList = this.lists.checkboxesOfAssignment;
+      container = '.checkboxesOfIssueAssignment';
+    }
+
+    if (checkboxesList.indexOf(value) === -1) {
+      checkboxesList.push(value);
+      $(container).append(
+        `<label style="font-size:16px;">
+          <input type="checkbox" name="issue-${filterType}" value="${value}"/> ${value}
+        </label><br>`
+      );
     }
   },
-  addTypeHtml: function (isseuType) {
-    if (jiraAddon.listOfCheckboxesOfType.indexOf(isseuType) == -1) {
-      jiraAddon.listOfCheckboxesOfType.push(isseuType);
-      $('.checkboxesOfIssueType').append(
-        '<label style="font-size:16px;">' +
-        '<input type="checkbox" name="issue-type" value="' + isseuType + '"/> ' + isseuType + '</label>' +
-        '<br>')
-    }
+
+  attachFilterEventHandlers() {
+    $('.checkboxesOfIssueType input[type="checkbox"]').on('change', () => this.updateSelectedFilters('type'));
+    $('.checkboxesOfIssueStatus input[type="checkbox"]').on('change', () => this.updateSelectedFilters('status'));
+    $('.checkboxesOfIssueAssignment input[type="checkbox"]').on('change', () => this.updateSelectedFilters('assignment'));
   },
-  filter: function () {
-    jiraAddon.listOfElements.each(function (ind, el) {
-      var elementInType = jiraAddon.selectedFiltersOfType.indexOf(jiraAddon.arrayIssueType[ind]) !== -1 ? true : false;
-      var elementInStatus = jiraAddon.selectedFiltersOfStatus.indexOf(jiraAddon.arrayIssueStatus[ind]) !== -1 ? true : false;
-      if (jiraAddon.selectedFiltersOfType.length == 0) {
-        elementInType = true;
-      }
-      if (jiraAddon.selectedFiltersOfStatus.length == 0) {
-        elementInStatus = true;
-      }
-      if (elementInType == true && elementInStatus == true) {
+
+  updateSelectedFilters(filterType) {
+    this.selectedFilters[filterType] = $(`.checkboxesOfIssue${capitalize(filterType)} input[type="checkbox"]:checked`).map((_, el) => el.value).get();
+    this.filterIssues();
+  },
+
+  filterIssues() {
+    this.lists.elements.each((index, el) => {
+      let matchesType = this.selectedFilters.type.length === 0 || this.selectedFilters.type.includes(this.lists.issueType[index]);
+      let matchesStatus = this.selectedFilters.status.length === 0 || this.selectedFilters.status.includes(this.lists.issueStatus[index]);
+      let matchesAssignment = this.selectedFilters.assignment.length === 0 || this.selectedFilters.assignment.includes(this.lists.issueAssignment[index]);
+
+      if (matchesType && matchesStatus && matchesAssignment) {
         $(el).show(100);
-      } 
-      else {
+      } else {
         $(el).hide(100);
       }
     });
+  },
+
+  addingHtml() {
+    let htmlContent = `
+      <div class="issues-wrap" style="position: absolute; top: 0; right: -14rem;">
+        <p style="font-size:16px;text-decoration: underline;"><strong>Issue Type:</strong></p>
+        <form class="checkboxesOfIssueType"></form>
+        <p style="font-size:16px;text-decoration: underline;"><strong>Issue Status:</strong></p>
+        <form class="checkboxesOfIssueStatus"></form>
+        <p style="font-size:16px;text-decoration: underline;"><strong>Issue Assignee:</strong></p>
+        <form class="checkboxesOfIssueAssignment"></form>
+      </div>
+    `;
+    $("#view-subtasks_heading").append(htmlContent);
   }
-}
+};
 
 jiraAddon.init();
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
